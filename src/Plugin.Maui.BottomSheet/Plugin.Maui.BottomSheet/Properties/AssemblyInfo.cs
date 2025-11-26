51305001a3912125ddd34a475cf92334d8210d91
Civale, Luca <Luca.Civale@sycor.de>
2025-11-26 14:07:12 +0100

    v10

--- src/Plugin.Maui.BottomSheet/Plugin.Maui.BottomSheet/Properties/AssemblyInfo.cs
+++ src/Plugin.Maui.BottomSheet/Plugin.Maui.BottomSheet/Properties/AssemblyInfo.cs
@@ -1,8 +1,7 @@
 using System.Runtime.CompilerServices;
 
-[assembly: XmlnsDefinition("http://pluginmauibottomsheet.com", "Plugin.BottomSheet")]
+[assembly: XmlnsDefinition("http://pluginmauibottomsheet.com", "Plugin.BottomSheet", AssemblyName = "Plugin.BottomSheet")]
 [assembly: XmlnsDefinition("http://pluginmauibottomsheet.com", "Plugin.Maui.BottomSheet")]
-[assembly: XmlnsDefinition("http://pluginmauibottomsheet.com", "Plugin.Maui.BottomSheet.Behaviors.BottomSheetPeekBehavior")]
 [assembly: XmlnsDefinition("http://pluginmauibottomsheet.com/platformconfiguration/android", "Plugin.Maui.BottomSheet.PlatformConfiguration.AndroidSpecific")]
 
 [assembly: InternalsVisibleTo("Plugin.BottomSheet.Tests.Maui.Unit")]
