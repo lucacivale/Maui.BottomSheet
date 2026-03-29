26da02c2f9cb204182ca6a50b2f90da99225cbbc
Civale, Luca <Luca.Civale@sycor.de>
2026-03-29 17:12:30 +0200

    [BUG] PeekView height measurement breaks Button text/image alignment

--- src/Plugin.Maui.BottomSheet/Configuration.cs
+++ src/Plugin.Maui.BottomSheet/Configuration.cs
@@ -11,4 +11,11 @@ public sealed class Configuration
     /// should be automatically copied to the bottom sheet when the bottom sheet is created from a page.
     /// </summary>
     public bool CopyPagePropertiesToBottomSheet { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the new peek height calculation should be used.
+    /// Old behavior => measure view height manually(leads to #235)
+    /// New behavior => use already computed View.DesiredSize.
+    /// </summary>
+    public bool UseNewPeekHeightCalculation { get; set; }
 }
